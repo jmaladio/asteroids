3ee7913ba40ce4e4d9e246df68b384f3d84dec4c
Julian Aladio <jmaladio@gmail.com>
2024-10-11 10:07:03 -0300

    feature: add player behaviour and groups

--- main.py
+++ main.py
@@ -7,18 +7,27 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0,0,0))
-        player.draw(screen) 
-        pygame.display.flip()
+        for sprite in updatable:
+            sprite.update(dt)
 
+        screen.fill((0,0,0))
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 
